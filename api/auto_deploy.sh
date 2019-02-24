9da16a0e324479f6029680c1c9f04be398fd07dc
John Saterfiel <john.saterfiel@oracle.com>
2019-02-24 15:35:58 -0500

    (saterfiel) fixing crontab info

--- api/auto_deploy.sh
+++ api/auto_deploy.sh
@@ -3,8 +3,9 @@
 # use a cron tab with
 # */1 * * * * /home/ubuntu/auto_deploy.sh > /home/ubuntu/cron.log 2>&1
 # at the top of the cron tab also put some environment variables to get it working
-#PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 #SHELL=/bin/bash
+#PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+#GOPATH=/home/ubuntu/.go
 #DOD_DB=<db conn string>
 #DOD_API_ROOT_DIR=/home/ubuntu/SSW690B/api
 cd ~/SSW690B;
