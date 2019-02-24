6a7dd70cd4b121e7bccd79215af51db805952547
John Saterfiel <john.saterfiel@oracle.com>
2019-02-24 01:38:58 -0500

    (saterfiel) initial work on api auth to db

--- api/src/api_authentication.go
+++ api/src/api_authentication.go
@@ -6,7 +6,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type LoginModel struct {
@@ -43,9 +46,67 @@ type SignupModel struct {
 	DoctorLicences []SignupDoctorLicences `json:"doctorLicences,omitempty"`
 }
 
+func dbUserLogin(e string, p string) AuthResponse {
+	dbUserClearSessions()
+
+	db := getDB()
+	if db == nil {
+		return AuthResponse{}
+	}
+	defer db.Close()
+
+	userIDSt, _ := db.Prepare("select USER_ID, ROLE, PASSW from `dod`.`USERS` u where u.`EMAIL` = ?")
+	defer userIDSt.Close()
+
+	var userID int
+	var role, pHash string
+	lisrerr := userIDSt.QueryRow(e).Scan(&userID, &role, &pHash)
+	if lisrerr != nil {
+		fmt.Println("1: ", lisrerr.Error())
+		return AuthResponse{}
+	}
+
+	if checkPassword(p, pHash) == false {
+		dbAuditAction(userID, "Login:Failure")
+		return AuthResponse{}
+	}
+
+	sessionID, _ := uuid.NewUUID()
+
+	sessionSt, _ := db.Prepare("insert into `dod`.`SESSIONS` (USER_ID, SESSION_ID, EXP_DT) values (?, ?, NOW() + INTERVAL 1 DAY)")
+	_, sessionStErr := sessionSt.Exec(userID, sessionID)
+	defer sessionSt.Close()
+	if sessionStErr != nil {
+		fmt.Println("2: ", sessionStErr.Error())
+		return AuthResponse{}
+	}
+
+	dbAuditAction(userID, "Login:Success")
+
+	return AuthResponse{
+		SessionID: sessionID.String(),
+		Role:      role,
+	}
+}
+
+func dbUserLogout(s string) {
+	dbUserClearSessions()
+
+	db := getDB()
+	if db == nil {
+		return
+	}
+	defer db.Close()
+
+	userIDSt, _ := db.Prepare("delete from dod.SESSIONS where `SESSION_ID` = ?")
+	defer userIDSt.Close()
+
+	//no audit needed here as logouts are not a security concern
+	userIDSt.Exec(s)
+}
+
 func LoginPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 
 	var input LoginModel
 
@@ -54,17 +115,20 @@ func LoginPost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	//handle login request
-	resp := AuthResponse{
-		SessionID: "12345",
-		Role:      "patient",
+
+	auth := dbUserLogin(input.Email, input.Password)
+
+	if auth.SessionID == "" {
+		http.Error(w, "Invalid credentials", 401)
+		return
 	}
-	json.NewEncoder(w).Encode(resp)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(auth)
 }
 
 func LogoutPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 
 	var input LogoutModel
 
@@ -73,11 +137,14 @@ func LogoutPost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+
+	dbUserLogout(input.SessionID)
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func SignupPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 
 	var input SignupModel
 
@@ -86,6 +153,9 @@ func SignupPost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+
 	//handle login request
 	resp := AuthResponse{
 		SessionID: "12345",
