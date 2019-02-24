3c85d560e181c74d7f023f9680fba5999df3fb92
tagerjump <45743053+tagerjump@users.noreply.github.com>
2019-02-23 20:07:30 -0500

    Add files via upload

---
+++ app/lib/sign_up_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:login/home_page.dart';
+
+class SignUpPage extends StatefulWidget {
+  static String tag = 'sign-up-page';
+  @override
+  _SignUpPageState createState() => new _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool _switchSelected=true; //维护单选开关状态
+
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final confirmPassword = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Confirm Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+
+
+    final signUpButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.lightBlueAccent,
+        child: Text('Sign Up', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final ifDoctorSwitch = new Row(children: <Widget>[
+      Text('I am a doctor'),
+
+      Switch(//传入value和onChanged,传入value按钮初始化状态,onChanged状态改变回调
+      value: _switchSelected,
+      activeColor: Colors.red,
+      onChanged: (value){
+        setState(() {//setState动态更新
+          _switchSelected=value;
+        });
+      },
+    )
+    ],);
+
+
+    final doctorOptions = new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          autofocus: false,
+          decoration: InputDecoration(
+            hintText: 'Email',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      )
+        )
+      ],);
+
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 8.0),
+            confirmPassword,
+            SizedBox(height: 8.0),
+            ifDoctorSwitch,
+
+            signUpButton
+          ],
+        ),
+      ),
+    );
+  }
+}
