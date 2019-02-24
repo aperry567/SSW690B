75b155493445ff90d5d8fb8d76ebf13687462769
aperry567 <36387436+aperry567@users.noreply.github.com>
2019-02-23 19:40:25 -0500

    Update users.sql

--- db/users.sql
+++ db/users.sql
@@ -1,1 +1,9 @@
 
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `creation_date` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
