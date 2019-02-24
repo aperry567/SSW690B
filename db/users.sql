03821be4cba994087aa34c7eb5b8ff3ccccf4e76
aperry567 <36387436+aperry567@users.noreply.github.com>
2019-02-24 13:51:53 -0500

    Revised the database script to create the users table

--- db/users.sql
+++ db/users.sql
@@ -1,9 +1,13 @@
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `creation_date` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `doctorsondemand`.`users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `patient` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `string` VARCHAR(45) NOT NULL,
+  `postalCode` VARCHAR(45) NOT NULL,
+  `phoneNumber` VARCHAR(45) NOT NULL,
+  `license` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`));
+
