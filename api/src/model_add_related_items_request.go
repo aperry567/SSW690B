e20935722b870b97c7b2231745bbcca8896650c7
John Saterfiel <john.saterfiel@oracle.com>
2019-03-15 20:09:03 -0400

    (saterfiel) fixing json field name

--- api/src/model_add_related_items_request.go
+++ api/src/model_add_related_items_request.go
@@ -3,6 +3,6 @@ package main
 type AddRelatedItemsRequest struct {
 	Title    string `json:"title"`
 	Subtitle string `json:"subtitle"`
-	DateTime string `json:"datetime"`
+	DateTime string `json:"dateTime"`
 	Details  string `json:"details"`
 }
