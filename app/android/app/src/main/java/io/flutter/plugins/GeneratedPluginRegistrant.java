58f744723c0abbbd362809738029dcac33be4668
Tony Hawkson <123248001@qq.com>
2019-04-05 20:31:47 -0400

    0.2.6

--- app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,6 +2,7 @@ package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.imagepicker.ImagePickerPlugin;
+import sqip.flutter.SquareInAppPaymentsFlutterPlugin;
 
 /**
  * Generated file. Do not edit.
@@ -12,6 +13,7 @@ public final class GeneratedPluginRegistrant {
       return;
     }
     ImagePickerPlugin.registerWith(registry.registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"));
+    SquareInAppPaymentsFlutterPlugin.registerWith(registry.registrarFor("sqip.flutter.SquareInAppPaymentsFlutterPlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
--- app/release/README.md
+++ app/release/README.md
@@ -1,7 +1,5 @@
-## Version Format
+# Version Format
 0.milestone.subversion
 
-## Update log
 0.2.3 Mar/12
-
 0.2.4 Mar/16
