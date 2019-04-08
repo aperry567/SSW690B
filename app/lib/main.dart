374d14129cb471835b819d69340dd42b529b0d13
John Saterfiel <john.saterfiel@oracle.com>
2019-04-08 08:00:25 -0400

    (saterfiel) refactor work on the home list.  Nothing is showing up though.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login/screen/login_page.dart';
-import 'package:login/screen/home_page.dart';
 import 'package:flutter/services.dart';
-
 
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
--- app/lib/screen/home_list.dart
+++ app/lib/screen/home_list.dart
@@ -5,11 +5,104 @@ import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:login/component/list_card.dart';
 import 'package:login/models/list_response.dart';
 
+class _HomePageListContainer extends StatefulWidget {
+  final String url;
+  
+  _HomePageListContainer(
+    this.url
+  ) : super(key: ValueKey([url]));
+  static String tag = 'profile-page';
 
+  @override
+  _HomePageListContainerState createState() => new _HomePageListContainerState(url);
+}
+
+class _HomePageListContainerState extends State<_HomePageListContainer> {
+  final String url;
+  bool isLoading = true;
+  ListResponse list; 
+
+  Image defaultImg = Image.asset('assets/alucard.jpg', width: 100,height: 100,fit: BoxFit.fill,);
+
+  static const apiAddress = "http://35.207.6.9:8080";
+
+  _HomePageListContainerState(this.url){
+    loadFeed();
+    print("_homepage List - " + this.url);
+  }
+
+  Future<Null> loadFeed() async {
+    await http.get(this.url)
+        .then((response) {
+      print("_homepage List - Response status: ${response.statusCode}");
+      // print("Response body: ${response.body}");
+      if(response.statusCode == 400) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+      else if(response.statusCode == 200){
+        Map<String, dynamic> result = jsonDecode(response.body);
+        if (this.mounted){
+          setState(() {
+            isLoading = false;
+            list = ListResponse.fromJson(result);
+          });
+        }
+      }
+    });
+  }
+
+  Widget build(BuildContext context) {
+    List<Widget> widgetList = [];
+    List<Widget> cardList = [];
+    if (list != null) {
+      print("building this thing");
+      var listItems = list.items;
+      cardList.add(SizedBox(height: 10,));
+      for(var i = 0; i < listItems.length; i++){
+        var item = listItems[i];
+        var _base64Imag = item.photo;
+        Image _image = defaultImg;
+        if(_base64Imag != ""){
+          const Base64Codec base64 = Base64Codec();
+          var _imageBytes = base64.decode(_base64Imag);
+          _image = Image.memory(_imageBytes, width: 100, height: 100,fit: BoxFit.fill,);
+        }
+        cardList.add(SizedBox(height: 10,));
+        cardList.add(ListCard(item.label,  item.dateTime, item.title, item.subtitle, item.details, _image,item.labelColor, apiAddress + item.detailLink));
+      }
+    }
+    var listView = ListView(
+      shrinkWrap: true,
+      padding: EdgeInsets.only(left: 24.0, right: 24.0),
+      children: cardList,
+
+    );
+    widgetList.add(LiquidPullToRefresh(
+      showChildOpacityTransition: false,
+      backgroundColor: Colors.red,
+      color:Colors.cyan[500],
+      height: 500,
+      onRefresh: () => loadFeed(),	// refresh callback
+      child: listView,		// scroll view
+    ));
+    widgetList.add(listView);
+    Stack stack = new Stack(
+      children: widgetList,
+    );
+    if (isLoading) {
+      widgetList.add(Center(
+        child: CircularProgressIndicator(),
+      ));
+    }
+
+    return stack;
+  }
+}
 
 class HomeListPage extends StatefulWidget {
   final String url;
-  
   
   HomeListPage(
     this.url
@@ -32,19 +125,12 @@ class _HomeListPageState extends State<HomeListPage> {
     }
     bool _is_loading = true;
 
-    List<Widget> card_list = [];
-    List<Widget> visit_card_list = [];
-    List<Widget> exam_card_list = [];
-    List<Widget> rx_card_list = [];
     DefaultTabController tabbar;
-    String list_filter = '';
+    ListResponse list;
+    String url = "";
 
     Future<Null> getProfile() async {
-      card_list = [];
-      visit_card_list = [];
-      exam_card_list = [];
-      rx_card_list = [];
-      var url = apiAddress + apiURL;
+      url = apiAddress + apiURL;
       await http.get(url)
             .then((response) {
           print("Response status: ${response.statusCode}");
@@ -53,44 +139,11 @@ class _HomeListPageState extends State<HomeListPage> {
             setState(() {
               _is_loading = false;
             });
-          }
-          else if(response.statusCode == 200){
-            Image defaultImg = Image.asset('assets/alucard.jpg', width: 100,height: 100,fit: BoxFit.fill,);
+          } else if(response.statusCode == 200){
             Map<String, dynamic> result = jsonDecode(response.body);
-            ListResponse list = ListResponse.fromJson(result);
-            // var child = tabbar.child as TabBar;
             if (this.mounted){
               setState(() {
-                //_doctorLicences_value = result['doctorLicences'];
-                var list_items = list.items;
-                //card_list.add(SizedBox(height: 10,));
-                for(var i = 0; i < list_items.length; i++){
-                  var item = list_items[i];
-                  var _base64Imag = item.photo;
-                  Image _image = defaultImg;
-                  if(_base64Imag != ""){
-                    const Base64Codec base64 = Base64Codec();
-                    var _imageBytes = base64.decode(_base64Imag);
-                    _image = Image.memory(_imageBytes, width: 100, height: 100,fit: BoxFit.fill,);
-                  }
-                  card_list.add(SizedBox(height: 10,));
-                  card_list.add(ListCard(item.label,  item.dateTime, item.title, item.subtitle, item.details, _image,item.labelColor, apiAddress + item.detailLink));
-                  switch(item.label){
-                    case 'Visit':
-                      visit_card_list.add(SizedBox(height: 10,));
-                      visit_card_list.add(ListCard(item.label, item.dateTime, item.title, item.subtitle, item.details, _image,item.labelColor, apiAddress + item.detailLink));
-                      break;
-                    case 'Exam':
-                      exam_card_list.add(SizedBox(height: 10,));
-                      exam_card_list.add(ListCard(item.label,  item.dateTime, item.title, item.subtitle, item.details, _image,item.labelColor, apiAddress + item.detailLink));
-                      break;
-                    case 'Rx':
-                      rx_card_list.add(SizedBox(height: 10,));
-                      rx_card_list.add(ListCard(item.label,  item.dateTime, item.title, item.subtitle, item.details, _image,item.labelColor, apiAddress + item.detailLink));
-                      break;
-                  }
-                  //print(item.label);
-                }
+                list = ListResponse.fromJson(result);
                 _is_loading = false;
               });
             }
@@ -104,61 +157,21 @@ class _HomeListPageState extends State<HomeListPage> {
         children: widgetList,
       );
 
-      final list_view = ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: card_list,
-      );
+      List<Tab> tabs = [];
+      List<Widget> tabViews = [];
+      if (list != null){
+        tabs.add(Tab(text: 'All'));
+        // tabs.add(Tab(text: 'Visit'));
+        // tabs.add(Tab(text: 'Exam'));
+        // tabs.add(Tab(text: 'Prescription'));
 
-      final list_view_visit = ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left: 24.0, right: 24.0),
-        children: visit_card_list,
-      );
-
-      final list_view_exam = ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left: 24.0, right: 24.0),
-        children: exam_card_list,
-      );
-
-      final list_view_rx = ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left: 24.0, right: 24.0),
-        children: rx_card_list,
-      );
-
-
-      final liquid0 = LiquidPullToRefresh(
-        showChildOpacityTransition: false,
-        color:Colors.cyan[500],
-        onRefresh: () => getProfile(),	// refresh callback
-        child: list_view,		// scroll view
-      );
-
-      final liquid1 = LiquidPullToRefresh(
-        showChildOpacityTransition: false,
-        color:Colors.cyan[500],
-        onRefresh: () => getProfile(),	// refresh callback
-        child: list_view_visit,		// scroll view
-      );
-
-      final liquid2 = LiquidPullToRefresh(
-        showChildOpacityTransition: false,
-        color:Colors.cyan[500],
-        onRefresh: () => getProfile(),	// refresh callback
-        child: list_view_exam,		// scroll view
-      );
-
-      final liquid3 = LiquidPullToRefresh(
-        showChildOpacityTransition: false,
-        color:Colors.cyan[500],
-        onRefresh: () => getProfile(),	// refresh callback
-        child: list_view_rx,		// scroll view
-      );
-      
+        tabViews.add(_HomePageListContainer(url + "&" + list.filters[0].value));
+        // tabViews.add(_HomePageListContainer(url + "&" + list.filters[1].value));
+        // tabViews.add(_HomePageListContainer(url + "&" + list.filters[2].value));
+        // tabViews.add(_HomePageListContainer(url + "&" + list.filters[3].value));
+      }
       tabbar = DefaultTabController(
-        length: 4,
+        length: tabViews.length,
         child: new Scaffold(
           appBar: new PreferredSize(
             preferredSize: Size.fromHeight(35),
@@ -177,12 +190,7 @@ class _HomeListPageState extends State<HomeListPage> {
                       indicatorColor: Colors.white,
                       labelColor: Colors.white,
                       labelStyle: _textStyleWhite,
-                      tabs: [
-                        Tab(text: 'All'),
-                        Tab(text: 'Visit'),
-                        Tab(text: 'Exam'),
-                        Tab(text: 'Prescription'),
-                      ],
+                      tabs: tabs,
                     ),
                   ),
                 )
@@ -190,12 +198,7 @@ class _HomeListPageState extends State<HomeListPage> {
             )
           ),
           body: TabBarView(
-            children: [
-              liquid0,
-              liquid1,
-              liquid2,
-              liquid3,
-            ],
+            children: tabViews,
           ),
         ),
       );
